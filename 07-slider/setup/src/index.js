71b6f9efafe13cafff12b05f5cb80959aec3fd2c
Walusimbi Herbert Henry <walherb@gmail.com>
2023-06-14 15:24:32 +0300

    Done with Some Projects-14-06-2023

--- 07-slider/setup/src/index.js
+++ 07-slider/setup/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import Alternative from './Alternative';
 import App from './App';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Alternative />
   </React.StrictMode>
 );
