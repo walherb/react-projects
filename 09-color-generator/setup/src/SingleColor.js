a43aa23ad8447216dd15176602d8bf8ffeca8d0d
Walusimbi Herbert Henry <walherb@gmail.com>
2023-06-15 09:47:40 +0300

    Color-Generator-15-06-2023

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -1,17 +1,31 @@
 import React, { useEffect, useState } from 'react';
-import rgbToHex from './utils';
+// import rgbToHex from './utils';
 
 const SingleColor = ({ rgb, weight, index, hexColor }) => {
   // console.log(hexColor);
   const [alert, setAlert] = useState(false);
   const bcg = rgb.join(',');
-  const hex = rgbToHex(...rgb);
+  // const hex = rgbToHex(...rgb);
   const hexValue = `#${hexColor}`;
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [alert]);
 
   return (
-    <article className={`color ${index > 10 && 'color-light'}`} style={{ backgroundColor: `rgb(${bcg})` }}>
+    <article
+      className={`color ${index > 10 && 'color-light'}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexValue);
+      }}
+    >
       <p className='percent-value'>{weight}%</p>
       <p className='color-value'>{hexValue}</p>
+      {alert && <p className='alert'>copied to clipboard</p>}
     </article>
   );
 };
