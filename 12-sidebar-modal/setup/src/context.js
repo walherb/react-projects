c89d1f75197e7f9bf79920f8748df928601c101b
Walusimbi Herbert Henry <walherb@gmail.com>
2023-06-23 13:50:40 +0300

    Edited Files-2023-06-23

--- 12-sidebar-modal/setup/src/context.js
+++ 12-sidebar-modal/setup/src/context.js
@@ -1,1 +1,35 @@
-import React, { useState, useContext } from 'react'
+import React, { createContext, useContext, useState } from 'react';
+
+const AppContext = createContext();
+
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <AppContext.Provider value={{ isModalOpen, isSidebarOpen, openModal, openSidebar, closeModal, closeSidebar }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+// custom hook
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
